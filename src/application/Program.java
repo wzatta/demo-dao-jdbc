1559927314869b4d4b503d4acc8e7785ea5b3229
Waldyr Zatta <wzjssa@gmail.com>
2022-10-23 16:05:17 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,19 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
 		Department obj = new Department(1,"Books");
-		System.out.println(obj);
-
+		
+		Seller seller = new Seller(1,"Bob", "bob@gmail.com", LocalDate.now(), 3000.00, obj);
+		
+		System.out.println(seller);
 	}
 
 }
